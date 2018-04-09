1870be7f59a4cc01db4c168b23e12e84dbed012a
rhenvar <garrickkrol@hotmail.com>
2018-04-08 23:05:48 -0500

    Completed Specifications searching

--- redis_layer.php
+++ redis_layer.php
@@ -8,7 +8,17 @@ include_once 'datalayer.php';
 require "predis/autoload.php";
 Predis\Autoloader::register();
 
+
 function get_redis_specifications($search_input, $sort_by, $functional_area) {
+    $functional_areas_table = [
+        "all" => "",
+        "uwmadison" => "University of Wisconsin - Madison",
+        "cirriculuminstruction" => "Curriculum & Instruction",
+        "facultystaff" => "Faculty & Staff",
+        "finance" => "Finance",
+        "research" => "Research",
+        "students" => "Students"
+    ];
     try {
          $redis = new Predis\Client(array(
             'host' => '10.128.127.156',
@@ -18,19 +28,26 @@ function get_redis_specifications($search_input, $sort_by, $functional_area) {
              // Load all specifications into Redis? 
          }
 
-         $results = $redis->get('all_specifications');
+         $results = json_decode($redis->get('all_specifications'));
          
          // Default Landing
-         if (strcmp('', $search_input) == 0) {
-             return json_decode($results);
+         // Still need to check for functional areas
+         if (strcmp('', $search_input) == 0 && strcmp('relevance', $sort_by) == 0) {
+             $filtered_array = array();
+             foreach ($results as $result) {
+                 if (contains_area($result->functional_areas, $functional_areas_table[$functional_area])) {
+                     array_push($filtered_array, $result);
+                 }
+             }
+             return $filtered_array;
          }
          else {
              if (strcmp('dates', $sort_by) == 0) {
-
+                 usort($results, "cmp_date");
+                 return $results;
              }
              if (strcmp('', $search_input) != 0 && strcmp('relevance', $sort_by) == 0) {
                  $filtered_array = array();
-                 $results = json_decode($results);
 
                  $input_array = explode(' ', $search_input);
                  foreach ($results as $result) {
@@ -46,7 +63,7 @@ function get_redis_specifications($search_input, $sort_by, $functional_area) {
                              }
                          }
                      }
-                     if ($word_occurrences > 0) {
+                     if ($word_occurrences > 0 && contains_area($result->functional_areas, $functional_areas_table[$functional_area])) {
                          $ratio = $word_occurrences / (float) $specification_word_count;
                          $result->ratio = $ratio;
                          array_push($filtered_array, $result);
@@ -69,6 +86,23 @@ function cmp_ratio($a, $b) {
         return 0;
     }
     return ($a_ratio < $b_ratio) ? 1 : -1;
+}
+
+function cmp_date($a, $b) {
+    $a_date = strtotime($a->last_revised);
+    $b_date = strtotime($b->last_revised);
+    return ($a_date > $b_date ? -1 : 1);
+}
+
+function contains_area($functional_areas, $area) {
+    if (strcmp("", $area) == 0) {
+        return true;
+    }
+    $pos = strpos($functional_areas, $area);
+    if ($pos === false) {
+        return false;
+    }
+    return true;
 }
 
 function specifications_set() {
